01c922e82d55adeac090005344f51ef7d5c24cc4
ahmadalghali <ahmad-mustafaalghali@hotmail.com>
2021-03-02 02:10:33 +0000

    comments page

--- js/comments.js
+++ js/comments.js
@@ -1,7 +1,133 @@
-let ideaTitle = document.getElementById("ideaTitle")
-let ideaAuthor = document.getElementById("ideaAuthor")
-let ideaDescription = document.getElementById("ideaDescription")
+$(document).ready(function () {
 
-let commentsTextArea = document.getElementById("ideaTitle")
-let ideaTitle = document.getElementById("ideaTitle")
-let ideaTitle = document.getElementById("ideaTitle")
+    let url = "https://theunibook.herokuapp.com"
+    // let url = "http://localhost:8080"
+
+
+    let ideaTitle = document.getElementById("ideaTitle")
+    let ideaAuthor = document.getElementById("ideaAuthor")
+    let ideaDescription = document.getElementById("ideaDescription")
+
+    let commentsTextArea = document.getElementById("commentsTextArea")
+
+    let btnPostComment = document.getElementById("btnPostComment")
+
+    let commentCount = document.getElementById("commentCount")
+
+
+    let ideaId = sessionStorage.getItem("IDEA_ID_READ_COMMENTS")
+
+    btnPostComment.addEventListener("click", postComment)
+
+
+    initPage()
+
+    async function getIdea() {
+        let idea = await fetch(`${url}/ideas/${ideaId}`).then(response => response.json())
+        return idea
+    }
+
+    function initPage() {
+        displayIdea()
+        displayComments()
+    }
+
+    async function postComment() {
+        let commentMessage = commentsTextArea.value.trim()
+
+        if (commentMessage.length > 500) {
+            toastr.warning("Comment has exceeded character limit of 500.")
+            return
+        }
+
+        let comment = {
+            description: commentMessage,
+            ideaId: ideaId,
+            authorId: session.user.id
+        }
+
+        let postCommentResponse = await post(`/comments`, JSON.stringify(comment))
+
+        if (postCommentResponse.message == "comment saved") {
+            toastr.info("Thanks for commenting")
+            displayComments()
+            commentsTextArea.value = ''
+
+        } else {
+            toastr.error("We apologise, Something went wrong, comment could not be posted.")
+        }
+    }
+
+    async function displayComments() {
+        let comments = await fetch(`${url}/comments?ideaId=${ideaId}`).then(response => response.json())
+
+        renderCommentsHTML(comments)
+
+
+    }
+
+
+
+    function renderCommentsHTML(comments) {
+        let commentsContainer = document.getElementById("commentsContainer")
+
+
+        let htmlString = ''
+
+        if (comments.length == 0) {
+            htmlString = "<h3 style='color: grey; margin-left: 30%;'>Be the first to comment!</h3>"
+        }
+
+        for (let comment of comments) {
+
+            htmlString += `
+                <div class="item comment">
+                    <div class="media">
+
+
+                        <div class="media-body">
+                            <div class="meta mb-1"><span class="comment"><b>${comment.authorName}</b></span></div>
+                            <br>
+                            <div class="intro"><b>${comment.description}</b></div>
+                        </div>
+
+                    </div>
+                </div>
+            <br>
+                <hr><br>
+            `
+
+
+
+
+
+        }
+
+        commentsContainer.innerHTML = htmlString
+        commentCount.innerText = comments.length
+    }
+
+
+    async function displayIdea() {
+        let idea = await getIdea()
+        ideaTitle.innerHTML = idea.title
+        ideaAuthor.innerHTML = idea.authorName
+        ideaDescription.innerHTML = idea.description
+    }
+
+
+
+    async function post(endpoint, data) {
+
+        console.log("Post request sent")
+
+        let response = await fetch(`${url}${endpoint}`, {
+            method: "POST",
+            headers: { "Content-type": "application/json" },
+            body: data
+        }).then(response => response.json());
+
+        return response;
+    }
+
+})

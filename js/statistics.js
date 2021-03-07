c1ee1f2379284a59a27874fe02b6ea815ff87d76
ahmadalghali <ahmad-mustafaalghali@hotmail.com>
2021-03-07 07:53:50 +0000

    a

---
+++ js/statistics.js
@@ -0,0 +1,13 @@
+let totalNumOfCommentsField = document.getElementById("totalNumOfCommentsField")
+let numOfAnonymousIdeasField = document.getElementById("numOfAnonymousIdeasField")
+let numOfIdeasWithNoCommentsField = document.getElementById("numOfIdeasWithNoCommentsField")
+
+
+
+
+
+let totalNumOfCommentsField = document.getElementById("totalNumOfCommentsField")
+let totalNumOfCommentsField = document.getElementById("totalNumOfCommentsField")
+let totalNumOfCommentsField = document.getElementById("totalNumOfCommentsField")
+let totalNumOfCommentsField = document.getElementById("totalNumOfCommentsField")
+let totalNumOfCommentsField = document.getElementById("totalNumOfCommentsField")

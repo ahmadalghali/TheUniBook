0baebb8fa7220d5e1fc90fd88fc66f4c3f86bb98
ahmadalghali <ahmad-mustafaalghali@hotmail.com>
2021-03-11 04:58:40 +0000

    a

--- js/change_password.js
+++ js/change_password.js
@@ -1,33 +1,32 @@
 $(document).ready(function () {
-    
-    //let url = "https://theunibook.herokuapp.com"
-    let url = "http://localhost:8080"
+
+    let url = "https://theunibook.herokuapp.com"
+    // let url = "http://localhost:8080"
 
     let changePasswordForm = document.getElementById("changePasswordForm")
     let session = JSON.parse(sessionStorage.getItem("session"));
     changePasswordForm.addEventListener("submit", changePassword)
 
-    async function changePassword(e){
+    async function changePassword(e) {
         e.preventDefault();
 
         let oldPassword = document.getElementById("old_password")
         let newPassword = document.getElementById("new_password")
         let confirmPassword = document.getElementById("confirm_password")
         let changePasswordResponse = await fetch(`${url}/changePassword?oldPassword=${oldPassword.value}&newPassword=${newPassword.value}&confirmPassword=${confirmPassword.value}&userId=${session.user.id}`, { method: "POST" }).then(response => response.json())
-        
-        if(newPassword.value != confirmPassword.value){
+
+        if (newPassword.value != confirmPassword.value) {
             toastr.error("Passwords do not match")
         }
-        else if(changePasswordResponse.message == "New password cannot be the same as old password"){
+        else if (changePasswordResponse.message == "New password cannot be the same as old password") {
             toastr.error(changePasswordResponse.message)
         }
 
-        else if(changePasswordResponse.message == "Password not changed")
-        {
+        else if (changePasswordResponse.message == "Password not changed") {
             toastr.error("Incorrect old password")
         }
-        else{
-                
+        else {
+
             toastr.success("Password changed successfully")
         }
     }
